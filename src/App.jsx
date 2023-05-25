6763bce6aa66a98b155005c4f2050d474d3d0b40
Gümrah Sindar <gsindar@gmail.com>
2023-05-25 23:24:34 +0300

    complete heading section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-
 import './App.css'
 
 function App() {
-
   return (
-    <>
-     Hello World
-    </>
+    <div className='container mx-auto mb-24 mt-20'>
+      <section
+        id='header'
+        className='mx-6 flex flex-col items-center justify-center text-center md:flex-row'
+      >
+        {/* Introducing */}
+        <div className=''>
+          <h1 className='mb-5 text-[40px] font-bold leading-8 tracking-tight text-veryDarkMagneta'>
+            10,000+ of our users love our products.
+          </h1>
+          <p className='text-[19px] font-medium leading-6 tracking-tight text-darkGrayishMagneta'>
+            We only provide great products combined with excellent customer
+            service. See what our satisfied customers are saying about our
+            services.
+          </p>
+        </div>
+        {/* Reviews */}
+        <div className=''></div>
+      </section>
+      <section id='card'></section>
+    </div>
   )
 }
 
