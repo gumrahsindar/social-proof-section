fa2d78b877a280927d60712cc0db5fe5c73b7e28
Gümrah Sindar <gsindar@gmail.com>
2023-05-25 23:23:31 +0300

    fix typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      color: {
-        veryDarkMagneta: "hsl(300, 43%, 22%)",
-        softPink: "hsl(333, 80%, 67%)",
-        darkGrayishMagneta: "hsl(303, 10%, 53%)",
-        lightGrayishMagneta: "hsl(300, 24%, 96%)",
+      colors: {
+        veryDarkMagneta: 'hsl(300, 43%, 22%)',
+        softPink: 'hsl(333, 80%, 67%)',
+        darkGrayishMagneta: 'hsl(303, 10%, 53%)',
+        lightGrayishMagneta: 'hsl(300, 24%, 96%)',
       },
     },
   },
   plugins: [],
-};
+}
