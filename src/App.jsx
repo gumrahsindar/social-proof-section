b91ef467477349286e200a4868e65418e3391ef6
Gümrah Sindar <gsindar@gmail.com>
2023-05-31 22:09:37 +0300

    add review section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import starIcon from '../public/images/icon-star.svg'
 import StarRating from './components/StarRating'
+import Reviews from './components/Reviews'
+import UserData from '../data/UserData'
 
 function App() {
   return (
-    <div className='container mx-auto mb-24 mt-20 max-w-7xl md:mt-28 md:-translate-x-10'>
+    <div className='container mx-auto mb-24 mt-20 h-screen max-w-[1440px] md:mt-28 md:-translate-x-16'>
       <section
         id='header'
-        className='mx-6 flex flex-col items-center justify-center gap-10 text-center md:mx-40 md:flex-row md:items-start md:gap-32 md:text-left'
+        className='mx-6 mb-12 flex  flex-col items-center justify-center gap-10 text-center md:mx-40 md:mb-[4.5rem] md:flex-row md:items-start md:gap-32 md:text-left'
       >
         {/* Introducing */}
-        <div className=''>
+        <div>
           <h1 className='mb-5 text-[40px] font-bold leading-8 tracking-tight text-veryDarkMagneta md:text-[56px] md:leading-[48px]'>
             10,000+ of our users love our products.
           </h1>
@@ -32,7 +34,6 @@ function App() {
             starIcon={starIcon}
             reviewText='Report Guru'
             translateX=' md:translate-x-[10%]'
-            spaceX='md:space-x-24'
           />
           <StarRating
             starIcon={starIcon}
@@ -41,7 +42,12 @@ function App() {
           />
         </ul>
       </section>
-      <section id='card'></section>
+      <section
+        id='card'
+        className='mx-6 mb-12 flex flex-col items-center justify-center gap-4 text-center md:mx-40 md:mb-[4.5rem] md:w-10/12 md:flex-row md:items-start md:gap-8 md:text-left'
+      >
+        <Reviews userData={UserData} />
+      </section>
     </div>
   )
 }
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,25 @@
+const Reviews = ({ userData }) => {
+  return (
+    <>
+      {userData.map((user) => (
+        <div
+          key={user.id}
+          style={{ transform: user.transform }}
+          className='rounded-xl bg-veryDarkMagneta px-8 py-10 text-white'
+        >
+          <div className='justify-senter mb-6 flex items-center gap-6 md:mb-8'>
+            <img src={user.src} alt='' className='w-12 rounded-full' />
+            <div className='flex flex-col items-start'>
+              <h3 className='text-[17px] font-bold'>{user.name}</h3>
+              <h4 className='text-[17px] text-softPink'>{user.status}</h4>
+            </div>
+          </div>
+          <p className='text-left text-[17px] font-medium leading-6 tracking-tight'>
+            {user.text}
+          </p>
+        </div>
+      ))}
+    </>
+  )
+}
+export default Reviews
