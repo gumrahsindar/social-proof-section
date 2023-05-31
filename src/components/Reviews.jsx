83e0134307c97431aa9e3dab3703f803b3ca9361
Gümrah Sindar <gsindar@gmail.com>
2023-05-31 22:32:57 +0300

    fix bottom margin

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,7 +5,7 @@ const Reviews = ({ userData }) => {
         <div
           key={user.id}
           style={{ transform: user.transform }}
-          className='rounded-xl bg-veryDarkMagneta px-8 py-10 text-white'
+          className='rounded-xl bg-veryDarkMagneta px-8 py-10 text-white last:mb-24'
         >
           <div className='justify-senter mb-6 flex items-center gap-6 md:mb-8'>
             <img src={user.src} alt='' className='w-12 rounded-full' />
