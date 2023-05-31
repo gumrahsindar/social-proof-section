59e68419216ac60523d4a4ebe48d46b7d44c9873
Gümrah Sindar <gsindar@gmail.com>
2023-05-31 22:08:24 +0300

    add user data array

---
+++ data/UserData.js
@@ -0,0 +1,27 @@
+const UserData = [
+  {
+    id: 1,
+    name: 'Colton Smith',
+    src: 'public/images/image-colton.jpg',
+    status: 'Verified Buyer',
+    text: '“ We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent! ”',
+  },
+  {
+    id: 2,
+    name: 'Irene Roberts',
+    src: 'public/images/image-irene.jpg',
+    status: 'Verified Buyer',
+    text: '“ Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.”',
+    transform: 'translateY(10%)',
+  },
+  {
+    id: 3,
+    name: 'Anne Wallace',
+    src: 'public/images/image-anne.jpg',
+    status: 'Verified Buyer',
+    text: '“ Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone! ”',
+    transform: 'translateY(20%)',
+  },
+]
+
+export default UserData
